70c8c031ff25db66b08497d87a738745758035bf
abo8 <8bo.alexandre@gmail.com>
2017-03-29 11:05:00 +0200

    Add files via upload
    CSS / JS

---
+++ gear/web/bundles/GearPlusBundle/js/main.js
@@ -0,0 +1,20 @@
+/*$(document).ready(function(){
+
+	$(window).load(function() {
+		$('body').removeClass('menu-open');
+		$("#loader").fadeOut("1000");
+	});
+
+	$('#burger').click(function(){
+		$('.menu nav').addClass('slowly');
+		$('body').addClass('menu-open');
+	});
+	
+	$('#cross, .menu-li').click(function(){
+		$('.menu nav').removeClass('slowly');
+		$('body').removeClass('menu-open');
+	});
+
+});
+*/
+
