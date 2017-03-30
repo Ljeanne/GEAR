fa724a55c3c2d645f4ef65540d1f4e14b1882930
Nicolas SERAIN <Nicolas SERAIN>
2017-03-31 01:22:56 +0200

    redirect when not logged

--- gear/src/GearPlusBundle/Controller/DefaultController.php
+++ gear/src/GearPlusBundle/Controller/DefaultController.php
@@ -26,15 +26,15 @@ class DefaultController extends Controller
         $length = count($elements);
         $i = 0;
         $tableau = [];
-        while ($i<5) {
-            $rand = rand(0, $length-1);
+        while ($i < 5) {
+            $rand = rand(0, $length - 1);
             $id1 = $elements[$rand];
             $repository = $this->getDoctrine()->getRepository(Product::class);
             $ele1 = $repository->findById($id1['id']);
             array_push($tableau, $ele1);
             $i++;
         }
-        return $this->render('GearPlusBundle:Default:index.html.twig', ['tableau'=>$tableau]);
+        return $this->render('GearPlusBundle:Default:index.html.twig', ['tableau' => $tableau]);
     }
 
     /**
@@ -44,88 +44,84 @@ class DefaultController extends Controller
     // Profil
     public function userObject()
     {
-        $userId=$this->getUser()->getId();
-        $allProducts = $this->getDoctrine()->getRepository(Product::class)->findBy(array('user'=>$userId));
+        $userId = $this->getUser()->getId();
+        $allProducts = $this->getDoctrine()->getRepository(Product::class)->findBy(array('user' => $userId));
 
-        $allFav = $this->getDoctrine()->getRepository(Favoris::class)->findBy(array('user'=>$userId));
-        return $this->render('GearPlusBundle:Default:user.html.twig',['tout_les_prod'=>$allProducts, 'tout_fav'=>$allFav]);
+        $allFav = $this->getDoctrine()->getRepository(Favoris::class)->findBy(array('user' => $userId));
+        return $this->render('GearPlusBundle:Default:user.html.twig', ['tout_les_prod' => $allProducts, 'tout_fav' => $allFav]);
     }
-
 
 
     /**
      * @Route("/annonces", name="annonces")
-
      */
     public function affProducts(Request $request)
     {
-        $repository = $this->getDoctrine()->getRepository('GearPlusBundle:Product');
-        $allprod = $repository->findAll();
+        if ($this->getUser()) {
+
+            $repository = $this->getDoctrine()->getRepository('GearPlusBundle:Product');
+            $allprod = $repository->findAll();
 
 
-        $new = new Product();
-        $form = $this->createForm(ProductType::class, $new);
-        $form->handleRequest($request);
-        $em = $this->getDoctrine()->getManager();
+            $new = new Product();
+            $form = $this->createForm(ProductType::class, $new);
+            $form->handleRequest($request);
+            $em = $this->getDoctrine()->getManager();
 
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $res = $form->getData();
+            if ($form->isSubmitted() && $form->isValid()) {
+                $res = $form->getData();
 
-            $res_title = '%'.$res->getTitle().'%';
-            $res_cat = $res->getCategory();
-            $res_prix = $res->getPrix();
-            $res_charisme = $res->getCharisme();
-            $res_intelligence = $res->getIntelligence();
-            $res_beaute = $res->getBeaute();
+                $res_title = '%' . $res->getTitle() . '%';
+                $res_cat = $res->getCategory();
+                $res_prix = $res->getPrix();
+                $res_charisme = $res->getCharisme();
+                $res_intelligence = $res->getIntelligence();
+                $res_beaute = $res->getBeaute();
 
-            $parameters=[];
-            
-            $query = "SELECT p FROM GearPlusBundle:Product p WHERE p.id <> 0 ";
-            if(isset($res_title) && $res_title != ''){
-                $query= $query."AND p.title LIKE :title ";
-               $parameters['title']=$res_title;
+                $parameters = [];
+
+                $query = "SELECT p FROM GearPlusBundle:Product p WHERE p.id <> 0 ";
+                if (isset($res_title) && $res_title != '') {
+                    $query = $query . "AND p.title LIKE :title ";
+                    $parameters['title'] = $res_title;
+                }
+                if (isset($res_cat) && $res_cat != null) {
+                    $query = $query . "AND p.category = :cat ";
+                    $parameters['cat'] = $res_cat;
+                }
+                if (isset($res_charisme)) {
+                    $query = $query . "AND p.charisme >= :charisme ";
+                    $parameters['charisme'] = $res_charisme;
+                }
+                if (isset($res_beaute)) {
+                    $query = $query . "AND p.beaute >= :beaute ";
+                    $parameters['beaute'] = $res_beaute;
+                }
+                if (isset($res_intelligence)) {
+                    $query = $query . "AND p.intelligence >= :intelligence ";
+                    $parameters['intelligence'] = $res_intelligence;
+                }
+                if (isset($res_prix)) {
+                    $query = $query . "AND p.prix <= :prix ";
+                    $parameters['prix'] = $res_prix;
+                }
+                //var_dump($query);
+                $sql = $em->createQuery($query);
+
+                foreach ($parameters as $key => $value) {
+                    $sql->setParameter($key, $value);
+                }
+
+                $find = $sql->getResult();
+                return $this->render('GearPlusBundle:Default:search.html.twig', ['products' => $find, 'form' => $form->createView()]);
             }
-            if (isset($res_cat) && $res_cat != null)
-            {
-                $query= $query."AND p.category = :cat ";
-                $parameters['cat']=$res_cat;
-            }
-            if(isset($res_charisme))
-            {
-                $query= $query."AND p.charisme >= :charisme ";
-                $parameters['charisme']=$res_charisme;
-            }
-            if(isset($res_beaute))
-            {
-                $query= $query."AND p.beaute >= :beaute ";
-                $parameters['beaute']=$res_beaute;
-            }
-            if(isset($res_intelligence))
-            {
-                $query= $query."AND p.intelligence >= :intelligence ";
-                $parameters['intelligence']=$res_intelligence;
-            }
-            if(isset($res_prix))
-            {
-                $query= $query."AND p.prix <= :prix ";
-                $parameters['prix']=$res_prix;
-            }
-            //var_dump($query);
-            $sql =  $em->createQuery($query);
 
-            foreach ($parameters as $key => $value){
-                $sql->setParameter($key , $value);
-            }
 
-            $find = $sql->getResult();
-            return $this->render('GearPlusBundle:Default:search.html.twig',['products'=>$find, 'form'=>$form->createView()]);
+            return $this->render('GearPlusBundle:Default:search.html.twig', ['form' => $form->createView(), 'products' => $allprod]);
+        } else {
+            return $this->redirectToRoute('fos_user_registration_register');
+
         }
-
-
-
-
-
-        return $this->render('GearPlusBundle:Default:search.html.twig',['form' => $form->createView(), 'products'=>$allprod]);
     }
 }
